a63f4682712c286faad3c238b627adbb7736c9fb
jkeiffer1 <saros82@hotmail.com>
2017-06-28 11:32:42 -0400

    added until, ending up with occassional infinite loop

--- code_review.rb
+++ code_review.rb
@@ -1,18 +1,23 @@
 
 a = [1,2,3,4,5,6,7,8,9].sample
 
-def guess
-	gets.chomp
+def guess()
+	gets.chomp.to_i
 end
 
-puts "do you know the answer?"
+puts "pick a number between 1 and 9"
 
-if guess == a
-	puts "congratulations"
-elsif guess >= a
+if guess >= a
 	puts "try lower"
 	guess
 elsif guess <= a
 	puts "try higher"
 	guess
+elsif guess == a
+	puts "congratulations"
+end
+until guess == a
+	if guess == a
+		puts "congratulations"
+	end
 end

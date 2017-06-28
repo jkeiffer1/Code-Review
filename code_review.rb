202a794a80d17188684dc0a42fb79504b4666f92
jkeiffer1 <saros82@hotmail.com>
2017-06-28 12:43:17 -0400

    finally works after switching to rand instead of sample

--- code_review.rb
+++ code_review.rb
@@ -1,18 +1,15 @@
 
-a = [1,2,3,4,5,6,7,8,9].sample
+a = rand(9)
 puts "pick a number between 1 and 9"
-def guess()
+
 guess = gets.chomp.to_i
-end
 until guess == a
-	if a <= guess
-	puts "try lower"
+	if guess < a
+	puts "try higher"
 	guess = gets.chomp.to_i
-	elsif a >= guess
-		puts "try higher"
+	elsif guess > a
+		puts "try lower"
 		guess = gets.chomp.to_i
 	end
 end
-
-	puts "congratulations!"
-
+	puts "Congratulations!"

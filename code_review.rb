93b46df1d2d7e28be5249b5931953fedb56c63db
jkeiffer1 <saros82@hotmail.com>
2017-06-28 11:57:52 -0400

    fixed stack error

--- code_review.rb
+++ code_review.rb
@@ -2,13 +2,13 @@
 a = [1,2,3,4,5,6,7,8,9].sample
 puts "pick a number between 1 and 9"
 def guess()
-guess == gets.chomp.to_i
+guess = gets.chomp.to_i
 end
 until guess == a
-	if guess >= a
+	if a <= guess
 	puts "try lower"
 	guess = gets.chomp.to_i
-	elsif guess <= a
+	elsif a >= guess
 		puts "try higher"
 		guess = gets.chomp.to_i
 	end
